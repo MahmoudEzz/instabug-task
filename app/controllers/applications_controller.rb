dd255d5d8bd33ac337c825f6df083f074b3b3a86
MahmoudEzz <mahmoud.ezz49@gmail.com>
2022-01-24 12:57:24 +0200

    Add redis to applcation creation

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -8,7 +8,7 @@ class ApplicationsController < ApplicationController
     render json: @applications, except: [:id]
   end
 
-  # GET /applications/1
+  # GET /applications/:token
   def show
     render json: @application, except: [:id]
   end
@@ -17,30 +17,50 @@ class ApplicationsController < ApplicationController
   def create
     @application = Application.new(application_params)
     if @application.save
+      app = {
+        :id => @application[:id],
+        :name => @application[:name],
+        :chats_count => @application[:chats_count],
+        :chats => []
+      }
+      serialized_app = JSON.generate(app)
+      $redis.set(@application[:token], serialized_app)
       render json: @application, except: [:id], status: :created, location: @application
     else
       render json: @application.errors, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /applications/1
+  # PATCH/PUT /applications/:token
   def update
     if @application.update(application_params)
+      app = {
+        :id => @application[:id],
+        :name => @application[:name],
+        :chats_count => @application[:chats_count]
+      }
+      serialized_app = JSON.generate(app)
+      $redis.set(@application[:token], serialized_app)
       render json: @application, except: [:id]
     else
       render json: @application.errors, status: :unprocessable_entity
     end
   end
 
-  # DELETE /applications/1
+  # DELETE /applications/:token
   def destroy
     @application.destroy
+    $redis.del(@application[:token])
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_application
-      @application = Application.find_by(token: params[:token])
+      serialized_app = $redis.get(params[:token])
+      @application = JSON.parse(serialized_app, {:symbolize_names => true})
+      if !@application
+        render plain: {error: "404 Application Not Found"}, status: 404
+      end
     end
 
     # Only allow a trusted parameter "white list" through.
